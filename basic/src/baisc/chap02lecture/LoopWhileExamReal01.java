19778c50578b694d4e604fdd9466432121d8339e
err-cho404 <snowleo456@gmail.com>
2024-10-17 14:32:41 +0900

    Add files via upload

---
+++ basic/src/baisc/chap02lecture/LoopWhileExamReal01.java
@@ -0,0 +1,24 @@
+package baisc.chap02lecture;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class LoopWhileExamReal01 {
+	public static void main(String[] args) {
+		Set setData = new HashSet(); 		//Set 자료구조는 중복을 제거하고, 순서가 없다.(순서를 알 수 없다.배열 index 순서가 없음)
+		setData.add("yangdoll");
+		setData.add("gildong");
+		setData.add("jjangdoll");
+		setData.add("kimparksea"); //set은 중복되는 자료를 제거할 수 있다.
+		
+		Iterator iter = setData.iterator();
+		while(iter.hasNext()) {  //주로 isnext나 hasnext가 쓰임
+			System.out.println(iter.next());  //->공식에 가까움 변수이름만 내마음대로 변경
+		}
+		
+//		for(;iter.hasNext();) {
+//			System.out.println(iter.next());
+//		}
+	}
+}
---
+++ basic/src/baisc/chap03lecture/IfExam0302.java
@@ -0,0 +1,34 @@
+package baisc.chap03lecture;
+
+import java.util.Scanner;
+
+public class IfExam0302 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int jumsu = 0;
+		boolean check = true;
+		while (check) {
+			System.out.println("점수를 입력[0~100]");
+			jumsu = scanner.nextInt();
+			if (jumsu < 101 && jumsu > -1) {
+				check = false;
+			}
+		}
+
+		if (jumsu >= 90 && jumsu < 101) {
+			System.out.println("100,000원 자동이체");
+		}
+		if (jumsu >= 80 && jumsu < 90) {
+			System.out.println("10,000원 자동이체");
+		}
+		if (jumsu >= 70 && jumsu < 80) {
+			System.out.println("1,000원 자동이체");
+		}
+		if (jumsu >= 60 && jumsu < 70) {
+			System.out.println("100원 자동이체");
+		}
+		if (jumsu >= 0 && jumsu < 60) {
+			System.out.println("등짝스매싱");
+		}
+	}
+}
